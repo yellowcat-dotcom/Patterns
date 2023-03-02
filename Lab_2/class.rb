f66b8faf1d0eaa49d23d49d288732e6a1d44de2f
Валентина Гончаренко <83706757+yellowcat-dotcom@users.noreply.github.com>
2023-03-03 00:34:26 +0300

    1_1

---
+++ Lab_2/class.rb
@@ -0,0 +1,18 @@
+class Student
+    # создаем методы чтения и записи attr_accessor
+    attr_accessor :id, :phone, :git, :telegram, :email, :last_name, :first_name, :paternal_name
+
+    # конструктор
+    def initialize(last_name, first_name, paternal_name, options = {})
+      self.last_name = last_name
+      self.first_name = first_name
+      self.paternal_name = paternal_name
+      self.id = options[:id]
+      self.phone = options[:phone]
+      self.git = options[:git]
+      self.telegram = options[:telegram]
+      self.email = options[:email]
+    end
+
+
+  end
