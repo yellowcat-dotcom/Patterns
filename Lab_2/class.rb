c5e1f4378a5e8cf7f0598d8b75feeabb00eb9eaf
Валентина Гончаренко <83706757+yellowcat-dotcom@users.noreply.github.com>
2023-03-07 15:44:55 +0300

    Добавила метод класса, проверяющий, является ли  некоторая строка телефонным номером. Потестила, работает, исключения появляются

--- Lab_2/class.rb
+++ Lab_2/class.rb
@@ -1,6 +1,12 @@
 class Student
     # создаем методы чтения и записи attr_accessor
-    attr_accessor :last_name, :first_name, :paternal_name, :id, :phone, :git, :telegram, :email
+    attr_accessor :last_name, :first_name, :paternal_name, :id, :git, :telegram, :email
+    attr_reader :phone
+
+    #валидатор телефона
+    def self.valid_phone?(phone)
+      phone.match(/^\+?[7,8] ?\(?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/)
+    end
 
     # конструктор
     def initialize(last_name, first_name, paternal_name, id:nil, phone:nil, git:nil, telegram:nil, email:nil)
@@ -12,6 +18,14 @@ class Student
       self.git = git
       self.telegram = telegram
       self.email = email
+    end
+
+
+    #сеттер
+    def phone=(phone)
+      raise ArgumentError, "Incorrect value: phone=#{phone}!" if !phone.nil? && !Student.valid_phone?(phone)
+
+      @phone = phone
     end
 
     def to_s
